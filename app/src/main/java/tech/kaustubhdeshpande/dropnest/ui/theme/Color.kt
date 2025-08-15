101e78030fd5816c556e096f8b7e8fb48c3c7d9f
Kaustubh Deshpande <162901396+deshkaustubh@users.noreply.github.com>
2025-08-15 15:23:39 +0530

    refactor: clean up imports and improve formatting in Color.kt and Theme.kt

--- app/src/main/java/tech/kaustubhdeshpande/dropnest/ui/theme/Color.kt
+++ app/src/main/java/tech/kaustubhdeshpande/dropnest/ui/theme/Color.kt
@@ -1,4 +1,5 @@
 package tech.kaustubhdeshpande.dropnest.ui.theme
+
 import androidx.compose.ui.graphics.Color
 
 val primaryLight = Color(0xFF276A49)
--- app/src/main/java/tech/kaustubhdeshpande/dropnest/ui/theme/Theme.kt
+++ app/src/main/java/tech/kaustubhdeshpande/dropnest/ui/theme/Theme.kt
@@ -1,17 +1,15 @@
 package tech.kaustubhdeshpande.dropnest.ui.theme
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
@@ -261,20 +259,20 @@ fun DropnestTheme(
     dynamicColor: Boolean = true,
     content: @Composable() () -> Unit
 ) {
-  val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = AppTypography,
-    content = content
-  )
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        content = content
+    )
 }
 
